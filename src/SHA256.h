573f6980af2b31ea4704f583dbe9514088f49482
jack <jackhuang1205@gmail.com>
2025-08-20 23:32:00 +0800

    Fix sha256 algorithm

--- src/SHA256.h
+++ src/SHA256.h
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstring>
 #include <string>
 #include <vector>
 
@@ -7,25 +8,19 @@ class SHA256 {
   typedef unsigned char byte_t;
 
 public:
+  SHA256(std::string Data)
+      : SHA256(reinterpret_cast<const byte_t *>(Data.data()), Data.size()) {}
+
+  SHA256(const char *Data)
+      : SHA256(reinterpret_cast<const byte_t *>(Data), strlen(Data)) {}
+
   SHA256(const byte_t *Data, size_t Len) {
     std::vector<byte_t> PaddingData = padding(Data, Len);
 
     for (size_t i = 0; i < PaddingData.size(); i += 64) {
-      std::vector<word_t> W(64, 0);
-
-      // Load the first 16 words from the padding data
-      for (size_t j = 0; j < 16; ++j)
-        for (size_t k = 0; k < 4; ++k)
-          W[j] |=
-              (static_cast<word_t>(PaddingData[i + j * 4 + k]) << (24 - k * 8));
-
-      // Extend the first 16 words into the remaining 48 words
-      for (size_t j = 16; j < 64; ++j) {
-        word_t s0 = (W[j - 15] >> 7) ^ (W[j - 15] >> 18) ^ (W[j - 15] >> 3);
-        word_t s1 = (W[j - 2] >> 17) ^ (W[j - 2] >> 19) ^ (W[j - 2] >> 10);
-        W[j] = W[j - 16] + s0 + W[j - 7] + s1;
-      }
-
+      std::vector<byte_t> Section(PaddingData.begin() + i,
+                                  PaddingData.begin() + i + 64);
+      std::vector<word_t> W = byteToWord(Section);
       update(W);
     }
   }
@@ -49,11 +44,19 @@ private:
     word_t a = H[0], b = H[1], c = H[2], d = H[3], e = H[4], f = H[5], g = H[6],
          h = H[7];
 
+    auto op_s0 = [this](word_t x) {
+      return rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22);
+    };
+
+    auto op_s1 = [this](word_t x) {
+      return rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25);
+    };
+
     for (size_t j = 0; j < 64; ++j) {
-      word_t S0 = (a >> 2) ^ (a >> 13) ^ (a >> 22);
+      word_t S0 = op_s0(a);
       word_t maj = (a & b) ^ (a & c) ^ (b & c);
       word_t temp2 = S0 + maj;
-      word_t S1 = (e >> 6) ^ (e >> 11) ^ (e >> 25);
+      word_t S1 = op_s1(e);
       word_t ch = (e & f) ^ (~e & g);
       word_t temp1 = h + S1 + ch + K[j] + W[j];
 
@@ -77,6 +80,36 @@ private:
     H[7] += h;
   }
 
+  std::vector<word_t> byteToWord(const std::vector<byte_t> &Section) {
+    std::vector<word_t> W(64, 0);
+
+    // Load the first 16 words from the data section
+    for (size_t i = 0; i < 16; ++i)
+      for (size_t j = 0; j < 4; ++j)
+        W[i] |= (static_cast<word_t>(Section[i * 4 + j]) << (24 - j * 8));
+
+    auto op_s0 = [this](word_t x) {
+      return rotr(x, 7) ^ rotr(x, 18) ^ (x >> 3);
+    };
+
+    auto op_s1 = [this](word_t x) {
+      return rotr(x, 17) ^ rotr(x, 19) ^ (x >> 10);
+    };
+
+    // Extend the first 16 words into the remaining 48 words
+    for (size_t j = 16; j < 64; ++j) {
+      word_t s0 = op_s0(W[j - 15]);
+      word_t s1 = op_s1(W[j - 2]);
+      W[j] = W[j - 16] + s0 + W[j - 7] + s1;
+    }
+
+    return W;
+  }
+
+  word_t rotr(word_t Value, int Shift) const {
+    return (Value >> Shift) | (Value << (32 - Shift));
+  }
+
   std::vector<byte_t> padding(const byte_t *Data, size_t Len) {
     size_t PaddingLen = Len + 9;
     if (PaddingLen % 64 != 0)
@@ -89,28 +122,31 @@ private:
     PaddingData[Len] = 0x80; // Append a single '1' bit (0x80 in hex)
 
     int Offset = 0;
-    while (Len) {
-      // Append the length of the original data in bits
-      PaddingData[PaddingLen - 1 - Offset] = Len & 0xff;
-      Len >>= 8;
+    size_t BitsLen = Len * 8;
+    for (int i = 0; i < 8; ++i) {
+      // Append the length of the original data in bits, in big-endian order
+      PaddingData[PaddingLen - 1 - i] = BitsLen & 0xff;
+      BitsLen >>= 8;
     }
-
     return PaddingData;
   }
 
-  word_t H[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-               0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
+  word_t H[8] = {
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+  };
 
   const word_t K[64] = {
-      0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
-      0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-      0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
-      0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-      0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
-      0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-      0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
-      0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-      0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
-      0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-      0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+    0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+    0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+    0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+    0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+    0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+  };
 };
